e6f495c2747698a3dd844983b48601c2a31d4d6e
gmskazi <98298538+gmskazi@users.noreply.github.com>
2025-04-04 21:52:31 +1000

    Autocomplete refactor (#61)
    * Moved autocomplete to its own file for testing and wrote some tests
    
    * modifying the completions gif to run faster

--- cmd/encrypt.go
+++ cmd/encrypt.go
@@ -5,9 +5,6 @@ package cmd
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
 
 	"github.com/gmskazi/pdfmc/cmd/pdf"
 	"github.com/gmskazi/pdfmc/cmd/ui/multiSelect"
@@ -97,79 +94,7 @@ func init() {
 	encryptCmd.Flags().StringP("password", "p", "", "Password to encrypt the PDF files.")
 
 	// autocomplete for files flag
-	mergeCmd.ValidArgsFunction = func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
-		var suggestions []string
-		var homeDir string
-		dir := "."
-
-		if strings.HasPrefix(toComplete, "/") {
-			dir = "/"
-			toComplete = strings.TrimPrefix(toComplete, "/")
-		} else if strings.HasPrefix(toComplete, "~/") {
-			var err error
-			homeDir, err = os.UserHomeDir()
-			if err != nil {
-				fmt.Println(errorStyle.Render(err.Error()))
-				return nil, cobra.ShellCompDirectiveError
-			}
-			dir = homeDir
-			toComplete = strings.TrimPrefix(toComplete, "~/")
-		}
-
-		// Determine the base directory and filter term
-		searchDir := filepath.Join(dir, toComplete)
-		if fileInfo, err := os.Stat(searchDir); err == nil && fileInfo.IsDir() {
-			dir = searchDir
-			toComplete = "" // Reset filtering because we are inside a valid folder
-		} else {
-			dir = filepath.Dir(searchDir) // Use the parent directory
-			toComplete = filepath.Base(searchDir)
-		}
-
-		// Track already selected PDFs
-		usedFiles := make(map[string]bool)
-		for _, arg := range args {
-			usedFiles[strings.TrimSpace(arg)] = true
-		}
-
-		// Read directory contents (1-level deep, no recursion)
-		files, err := os.ReadDir(dir)
-		if err != nil {
-			return nil, cobra.ShellCompDirectiveError
-		}
-
-		for _, file := range files {
-			name := file.Name()
-			lowerName := strings.ToLower(name)
-			fullPath := filepath.Join(dir, name)
-
-			// Convert home paths back to `~/`
-			if homeDir != "" && strings.HasPrefix(fullPath, homeDir) {
-				fullPath = "~/" + strings.TrimPrefix(fullPath, homeDir+"/")
-			}
-
-			// If no args, suggest directories and PDFs
-			if len(args) == 0 {
-				if (file.IsDir() || strings.HasSuffix(lowerName, ".pdf")) && strings.HasPrefix(lowerName, strings.ToLower(toComplete)) {
-					suggestions = append(suggestions, fullPath)
-				}
-			} else {
-				// If args exist, only suggest PDFs that haven't been selected yet
-				if !file.IsDir() && strings.HasSuffix(lowerName, ".pdf") && strings.HasPrefix(lowerName, strings.ToLower(toComplete)) {
-					if !usedFiles[fullPath] {
-						suggestions = append(suggestions, fullPath)
-					}
-				}
-			}
-		}
-
-		if len(suggestions) == 0 {
-			return nil, cobra.ShellCompDirectiveNoFileComp
-		}
-		// Prevent spaces from being inserted after completing a directory
-		return suggestions, cobra.ShellCompDirectiveNoSpace | cobra.ShellCompDirectiveDefault
-	}
-
+	mergeCmd.ValidArgsFunction = GetSuggestions
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
