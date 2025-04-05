cfa3dd5878e4e34b6e1c78e75f2702595e224ecf
Aito Nakajima <gmskazi@gmail.com>
2025-04-05 22:58:38 +1000

    Making encrypt.go as small as possible, might need to add more helper files specific to its module

--- cmd/encrypt.go
+++ cmd/encrypt.go
@@ -4,9 +4,6 @@ Copyright © 2025 NAME HERE Aito Nakajima
 package cmd
 
 import (
-	"github.com/gmskazi/pdfmc/cmd/pdf"
-	"github.com/gmskazi/pdfmc/cmd/ui/multiSelect"
-	"github.com/gmskazi/pdfmc/cmd/utils"
 	"github.com/spf13/cobra"
 )
 
@@ -17,49 +14,9 @@ var encryptCmd = &cobra.Command{
 	Long:  `This is a tool for encrypting PDF files.`,
 	Args:  cobra.ArbitraryArgs,
 	Run: func(cmd *cobra.Command, args []string) {
-		var (
-			selectedPdfs []string
-			pword        string
-			quit         bool
-		)
-		const encrypt = "encrypt"
-
-		fileUtils := utils.NewFileUtils(args)
-		pdfProcessor := pdf.NewPDFProcessor(fileUtils, encrypt)
-
-		// check if any files/folders are provided
-		pdfs, dir, interactive, err := fileUtils.CheckProvidedArgs()
-		if handleError(cmd, err) {
+		if err := executeEncrypt(cmd, args); err != nil {
 			return
 		}
-
-		if interactive {
-			selectedPdfs, quit, err = multiSelect.MultiSelectInteractive(pdfs, dir, encrypt)
-			if handleError(cmd, err) || quit {
-				return
-			}
-
-			if len(selectedPdfs) == 0 {
-				cmd.Println(infoStyle.Render("No PDFs were selected. Exiting."))
-				return
-			}
-		}
-
-		pword, quit, err = getPassword(cmd)
-		if handleError(cmd, err) || quit {
-			return
-		}
-
-		if !interactive {
-			selectedPdfs = pdfs
-		}
-
-		saveDir, err := fileUtils.GetCurrentWorkingDir()
-		if handleError(cmd, err) {
-			return
-		}
-
-		processPDFs(cmd, pdfProcessor, selectedPdfs, dir, saveDir, pword)
 	},
 }
 
@@ -67,9 +24,6 @@ func init() {
 	rootCmd.AddCommand(encryptCmd)
 
 	encryptCmd.Flags().StringP("password", "p", "", "Password to encrypt the PDF files.")
-	if err := encryptCmd.Parent().MarkFlagRequired("password"); err != nil {
-		return
-	}
 
 	// autocomplete for files flag
 	mergeCmd.ValidArgsFunction = GetSuggestions
--- cmd/helpers.go
+++ cmd/helpers.go
@@ -4,41 +4,88 @@ import (
 	"fmt"
 
 	"github.com/gmskazi/pdfmc/cmd/pdf"
+	"github.com/gmskazi/pdfmc/cmd/ui/multiSelect"
 	textInputs "github.com/gmskazi/pdfmc/cmd/ui/textinputs"
+	"github.com/gmskazi/pdfmc/cmd/utils"
 	"github.com/spf13/cobra"
 )
 
-func handleError(cmd *cobra.Command, err error) bool {
+func handleError(cmd *cobra.Command, err error) error {
 	if err != nil {
 		cmd.PrintErrln(errorStyle.Render(err.Error()))
-		return true
+		return err
 	}
-	return false
+	return nil
 }
 
-func getPassword(cmd *cobra.Command) (string, bool, error) {
+func getPassword(cmd *cobra.Command) (string, error) {
 	flagPassword, err := cmd.Flags().GetString("password")
 	if err != nil {
-		return "", false, err
+		return "", err
 	}
 	if flagPassword == "" {
 		pword, quit, err := textInputs.TextinputInteractive()
-		if err != nil {
-			return "", false, err
+		if err != nil || quit {
+			return "", err
 		}
-		return pword, quit, nil
+		return pword, nil
 	}
-	return flagPassword, false, nil
+	return flagPassword, nil
 }
 
 func processPDFs(cmd *cobra.Command, pdfProcessor *pdf.PDFProcessor, selectedPdfs []string, dir, saveDir, pword string) {
 	for _, pdf := range selectedPdfs {
 		encryptedPdf, err := pdfProcessor.EncryptPdf(pdf, dir, pword)
-		if handleError(cmd, err) {
+		if err = handleError(cmd, err); err != nil {
 			return
 		}
 
 		complete := fmt.Sprintf("PDF file encrypted successfully to: %s/%s", saveDir, encryptedPdf)
 		cmd.Println(selectedStyle.Render(complete))
 	}
+}
+
+func executeEncrypt(cmd *cobra.Command, args []string) error {
+	var (
+		selectedPdfs []string
+		pword        string
+		quit         bool
+	)
+
+	fileUtils := utils.NewFileUtils(args)
+	pdfProcessor := pdf.NewPDFProcessor(fileUtils, "encrypt")
+
+	pdfs, dir, interactive, err := fileUtils.CheckProvidedArgs()
+	if err = handleError(cmd, err); err != nil {
+		return err
+	}
+
+	if interactive {
+		selectedPdfs, quit, err = multiSelect.MultiSelectInteractive(pdfs, dir, "encrypt")
+		if err = handleError(cmd, err); err != nil || quit {
+			return err
+		}
+
+		if len(selectedPdfs) == 0 {
+			cmd.Println(infoStyle.Render("No PDFs were selected. Exiting."))
+			return err
+		}
+	}
+
+	pword, err = getPassword(cmd)
+	if err = handleError(cmd, err); err != nil {
+		return err
+	}
+
+	if !interactive {
+		selectedPdfs = pdfs
+	}
+
+	saveDir, err := fileUtils.GetCurrentWorkingDir()
+	if err = handleError(cmd, err); err != nil {
+		return err
+	}
+
+	processPDFs(cmd, pdfProcessor, selectedPdfs, dir, saveDir, pword)
+	return nil
 }
